7543dd3e37ff7cccb828335c15de9c2753e916bf
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2024-11-16 13:09:37 +0400

    Install and activate daisyui

--- resources/js/Components/Core/InputLabel.tsx
+++ resources/js/Components/Core/InputLabel.tsx
@@ -1,20 +1,18 @@
-import { LabelHTMLAttributes } from 'react';
+import {LabelHTMLAttributes} from 'react';
 
-export default function InputLabel({
+export default function InputLabel(
+  {
     value,
     className = '',
     children,
     ...props
-}: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
-    return (
-        <label
-            {...props}
-            className={
-                `block text-sm font-medium text-gray-700 dark:text-gray-300 ` +
-                className
-            }
-        >
-            {value ? value : children}
-        </label>
-    );
+  }: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
+  return (
+    <label
+      {...props}
+      className={`label ` + className}
+    >
+      <span className={"label-text"}>{value ? value : children}</span>
+    </label>
+  );
 }
