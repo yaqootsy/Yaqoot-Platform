e0cfde8d51259ae656951d37cf2a11deabf83841
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-05-06 18:07:59 +0400

    Add CountrySeeder inside DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DepartmentSeeder::class,
             CategorySeeder::class,
+            CountrySeeder::class
         ]);
     }
 }
