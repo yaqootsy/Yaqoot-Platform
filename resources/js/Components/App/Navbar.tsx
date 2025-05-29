5251693cc4ed61130db01f8d7e11efaa7fea286f
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-05-29 13:31:19 +0400

    Fix navbar user profile dropdown z-index issue

--- resources/js/Components/App/Navbar.tsx
+++ resources/js/Components/App/Navbar.tsx
@@ -64,7 +64,7 @@ function Navbar() {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1000] mt-3 w-52 p-2 shadow">
               <li>
                 <Link href={route('profile.edit')} className="justify-between">
                   Profile
