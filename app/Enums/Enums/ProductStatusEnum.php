acbfceeae7bef1283774b058db55e257b731823f
Nadim Alzoubi <nadim.alzoubi.99@gmail.com>
2025-06-24 15:59:31 +0400

    completed Product CRUD

---
+++ app/Enums/Enums/ProductStatusEnum.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums\Enums;
+
+enum ProductStatusEnum: string
+{
+    case Draft = 'draft';
+    case Published = 'published';
+
+    public static function labels(): array
+    {
+        return [
+            self::Draft->value => __('Draft'),
+            self::Published->value => __('Published'),
+        ];
+    }
+
+    public static function colors(): array
+    {
+        return [
+            'gray' => self::Draft->value,
+            'success' => self::Published->value,
+        ];
+    }
+}
